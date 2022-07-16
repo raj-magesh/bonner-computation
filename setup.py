a374d405fa83cf2a1a3db612e4cdb902602cfdf6
Raj Magesh Gauthaman <raj@raj-magesh.org>
2022-07-16 19:10:48 -0400

    fix: switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-requirements = (
-    "pandas",
-    "xarray",
-)
-
-setup(
-    name="bonner-computation",
-    version="0.1.0",
-    packages=find_namespace_packages(),
-    install_requires=requirements,
-)
+setup()
